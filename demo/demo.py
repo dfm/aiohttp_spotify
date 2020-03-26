cc87c49f42559911eec8b9516caa015fcacd63fb
Dan F-M <foreman.mackey@gmail.com>
2020-03-26 14:24:10 -0400

    comments in the demo app

--- demo/demo.py
+++ demo/demo.py
@@ -1,9 +1,9 @@
-import argparse
 import base64
 from functools import wraps
-from typing import AsyncIterator, Awaitable, Callable
+from typing import Any, AsyncIterator, Awaitable, Callable, Mapping
 
 import aiohttp_session
+import toml
 from aiohttp import ClientSession, web
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from cryptography import fernet
@@ -12,24 +12,11 @@ import aiohttp_spotify
 from aiohttp_spotify import SpotifyAuth
 
 
-async def client_session(app: web.Application) -> AsyncIterator[None]:
-    async with ClientSession() as session:
-        app["client_session"] = session
-        yield
-
-
-def parse_config() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--client_id", required=True)
-    parser.add_argument("--client_secret", required=True)
-    parser.add_argument("--redirect_uri", required=True)
-    parser.add_argument("-p", "--port", type=int, default=5000)
-    return parser.parse_args()
-
-
 def require_auth(
     handler: Callable[[web.Request, SpotifyAuth], Awaitable[web.Response]]
 ) -> Callable[[web.Request], Awaitable[web.Response]]:
+    """A decorator requiring that the user is authenticated to see a view"""
+
     @wraps(handler)
     async def wrapped(request: web.Request) -> web.Response:
         session = await aiohttp_session.get_session(request)
@@ -47,11 +34,13 @@ def require_auth(
 
 
 async def index(request: web.Request) -> web.Response:
-    return web.Response(body="hi!")
+    """A placeholder index page"""
+    return web.Response(body="here we are.")
 
 
 @require_auth
 async def me(request: web.Request, auth: SpotifyAuth) -> web.Response:
+    """View to show the authorized user's Spotify user information"""
     response = await request.app["spotify_app"]["spotify_client"].request(
         request.app["client_session"], auth, "/me"
     )
@@ -63,9 +52,24 @@ async def me(request: web.Request, auth: SpotifyAuth) -> web.Response:
     return web.json_response(response.json())
 
 
-async def update_auth(request: web.Request, auth: aiohttp_spotify.SpotifyAuth):
+async def update_auth(
+    request: web.Request, auth: aiohttp_spotify.SpotifyAuth
+) -> None:
+    """Handle updating the stored authorization information"""
     session = await aiohttp_session.get_session(request)
     session["spotify_auth"] = auth
+
+
+async def client_session(app: web.Application) -> AsyncIterator[None]:
+    """A fixture to create a single ClientSession for the app to use"""
+    async with ClientSession() as session:
+        app["client_session"] = session
+        yield
+
+
+def parse_config() -> Mapping[str, Any]:
+    """Load a TOML configuration file"""
+    return toml.load("config.toml")
 
 
 def app_factory(
@@ -73,9 +77,11 @@ def app_factory(
 ) -> web.Application:
     app = web.Application()
 
+    # Add the main routes to the app
     app.router.add_get("/", index, name="index")
     app.router.add_get("/me", me, name="me")
 
+    # Set up the Spotify app to instigate the OAuth flow
     app["spotify_app"] = aiohttp_spotify.spotify_app(
         client_id=client_id,
         client_secret=client_secret,
@@ -89,6 +95,7 @@ def app_factory(
     secret_key = base64.urlsafe_b64decode(fernet.Fernet.generate_key())
     aiohttp_session.setup(app, EncryptedCookieStorage(secret_key))
 
+    # Add the ClientSession to the app
     app.cleanup_ctx.append(client_session)
 
     return app
@@ -98,7 +105,9 @@ if __name__ == "__main__":
     config = parse_config()
     web.run_app(
         app_factory(
-            config.client_id, config.client_secret, config.redirect_uri
+            config["client_id"],
+            config["client_secret"],
+            config["redirect_uri"],
         ),
-        port=config.port,
+        port=config["port"],
     )
